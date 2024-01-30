3d500c1fa5fca80c606e27ac69c822ff96256983
Abhishek Kumar Verma <abhishek27501@gmail.com>
2024-01-30 22:45:55 +0530

    card section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,42 @@ import Section1 from './components/Section1'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   const data = [
     {
-      text:"Stories tailored to your taste",
-      imgUrl:""
+      text: "Stories tailored to your taste",
+      image:"https://images.unsplash.com/photo-1556848527-f7c548b972b2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhhbmR8ZW58MHx8MHx8fDA%3D"
     },
     {
-      text:"card 2"
-    }
+      text: "Cancel or switch plans anytime",
+      image:"https://images.unsplash.com/photo-1596633607590-7156877ef734?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2hha2luZyUyMGhhbmR8ZW58MHx8MHx8fDA%3D"
+    },
+    {
+      text: "A place just for kids",
+      image:"https://images.unsplash.com/photo-1617114890850-421519e5eabb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJlZCUyMGhlYXJ0fGVufDB8fDB8fHww"
+    },
+    {
+      text: "For your phone, tablet, laptop, and TV",
+      image:"https://images.unsplash.com/photo-1423666639041-f56000c27a9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9uaXRlciUyMHdpdGglMjBtb2JpbGV8ZW58MHx8MHx8fDA%3D"
+    },
   ]
 
   return (
     <>
-     <div className="bg-black">
+      <div className="bg-black">
         <Section1 />
-        {data.map((item , index) => (
-          <Card  data = {item}/>
-        ))}
-        
-     </div>
+
+        <div className="text-white w-[90%] m-auto mt-10">
+          <h3 className='text-3xl px-10 py-5 font-semibold'>More reasons to join</h3>
+          <div className="flex justify-center items-center flex-wrap gap-10 py-10">
+            {data.map((item, index) => (
+              <Card data={item} />
+            ))}
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import hand from '/hand.svg'
+import doublehand from '/doublehand.svg'
 
 function Card({data, index}) {
   return (
-    <>     
-      <div className="text-white w-[90%] flex justify-center items-center m-auto">
-        <div className="h-96 w-64 rounded-xl  bg-gradient-to-tl from-pink-800/20 via-[#1C1D3A] to-[#1C1D3A]">
-            <h2 className='text-3xl font-semibold px-2 py-5'>{data.text}</h2>
+    <>      
+      <div className="h-80 flex-grow w-64 px-5 rounded-3xl bg-gradient-to-tl from-pink-800/20 via-[#1E1629] to-[#1C1D3A] relative">
+            <h2 className='text-4xl font-bold px-2 py-5'>{data.text}</h2>
+            <img className='h-20 w-20 object-cover object-center absolute bottom-4 right-4 mix-blend-difference' src={data.image} alt="loading......." />
         </div>
-      </div>
     </>
   )
 }
