b7be4090a24e73f7837a375b1963253959211338
Abhishek Kumar Verma <abhishek27501@gmail.com>
2024-02-08 22:01:13 +0530

    footer section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,40 @@ import React from 'react'
 function Footer() {
   return (
     <>
-        <div>
-           <h1 className='text-5xl'>Questions? Call 000-800-919-1694 </h1> 
-        </div> 
+        <footer className='w-full py-10'>
+           <h1 className='text-gray-400 px-24'>Questions? Call 000-800-919-1694 </h1> 
+            <div className='flex justify-between space-y-16 flex-wrap px-10 md:w-[90%] text-gray-400 m-auto py-5'>
+                <div className='flex justify-around flex-grow items-center w-full md:w-1/2'>
+                   
+                    <div className="flex flex-col gap-y-3 underline">
+                        {["FAQ","Investor Relations","Privacy","Speed Test"].map((item, index) => (
+                            <a href="" key={index}>{item}</a>
+                        ))}
+                    </div>
+
+                    <div className="flex flex-col gap-y-3 underline">
+                        {["Help Center","Jobs","Cookies Preferences","Legal Notice"].map((item, index) => (
+                            <a href="" key={index}>{item}</a>
+                        ))}
+                    </div>
+                                        
+                </div>
+                
+                <div className='flex justify-around flex-grow items-center w-full md:w-1/2'>
+                    <div className="flex flex-col gap-y-3 underline">
+                        {["FAQ","Investor Relations","Privacy","Speed Test"].map((item, index) => (
+                            <a href="" key={index}>{item}</a>
+                        ))}
+                    </div>
+
+                    <div className="flex flex-col gap-y-3 underline">
+                        {["Help Center","Jobs","Cookies Preferences","Legal Notice"].map((item, index) => (
+                            <a href="" key={index}>{item}</a>
+                        ))}
+                    </div>      
+                </div>
+            </div>
+        </footer> 
     </>
   )
 }
